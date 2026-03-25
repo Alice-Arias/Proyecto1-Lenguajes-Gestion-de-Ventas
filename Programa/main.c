0c59a73675de7e7e4fd4853b427813e377fba5bd
LuisK19 <luis.trejos10@gmail.com>
2026-03-25 02:03:13 -0600

    Arreglos y implementacion de colores en la terminal

--- Programa/main.c
+++ Programa/main.c
@@ -3,9 +3,12 @@
 #include "cliente/cliente.h"
 #include "include/sitio.h"
 #include "include/evento.h"
+#include "include/sector.h"
+#include "include/asiento.h"
+#include "include/colors.h"
 
 void probarEstructuras() {
-    printf("\n=== Probando estructuras ===\n");
+    printf(COLOR_BLUE "\n=== Probando estructuras ===\n" COLOR_RESET);
 
     // Crear sitio
     agregarSitio("Estadio Nacional", "Santiago, Chile", "www.estadio.cl");
@@ -23,37 +26,36 @@ void probarEstructuras() {
     mostrarAsientos(sitios[0].sectores[0].asientos, 10);
     mostrarEventos();
 
-    printf("\n=== Fin de prueba ===\n");
+    printf(COLOR_BLUE "\n=== Fin de prueba ===\n" COLOR_RESET);
 }
 
 int main() {
-
-    cargarUsuarios(); // cargar datos a memoria
+    cargarUsuarios();
     inicializarSitios();
     inicializarEventos();
 
     int opcion;
 
     do {
-        printf("\n========= SISTEMA DE BOLETOS =========\n");
-        printf("1. Administrador\n");
-        printf("2. Cliente\n");
-        printf("4. Probar Estructuras\n");
-        printf("5. Salir\n");
+        // Título con color cian y negrita
+        printf(MENU_TITLE "\n========= SISTEMA DE BOLETOS =========\n" COLOR_RESET);
+        // Opciones en verde
+        printf(MENU_OPTION "1. Administrador\n" COLOR_RESET);
+        printf(MENU_OPTION "2. Cliente\n" COLOR_RESET);
+        printf(MENU_OPTION "4. Probar Estructuras\n" COLOR_RESET);
+        printf(MENU_OPTION "5. Salir\n" COLOR_RESET);
         printf("======================================\n");
-
 
         printf("Seleccione: ");
         scanf("%d", &opcion);
 
         switch(opcion) {
-
             case 1:
                 if (IniciarSesionAdmin()) {
-                    printf("Acceso concedido\n");
+                    printf(MENU_SUCCESS "Acceso concedido\n" COLOR_RESET);
                     menuAdmin();
                 } else {
-                    printf("Acceso denegado\n");
+                    printf(MENU_ERROR "Acceso denegado\n" COLOR_RESET);
                 }
                 break;
 
@@ -70,14 +72,12 @@ int main() {
                 break;
 
             default:
-                printf("Opcion invalida\n");
+                printf(MENU_ERROR "Opcion invalida\n" COLOR_RESET);
         }
-
     } while(opcion != 5);
 
     liberarUsuarios();
     liberarSitios();
     liberarEventos();
-
     return 0;
 }
